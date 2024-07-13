6d5e7a7ff319fdc803f48742da9405fa5219c082
Honami Ota <h.oota0705@gmail.com>
2024-07-14 06:18:20 +0900

    tasks_controller created

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TodoListController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,8 +19,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('/list', [TodoListController::class, 'index']);
+
+Route::resource('tasks', TaskController::class);
 
 
 
